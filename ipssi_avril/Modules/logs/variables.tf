4c9cbef4ce6e4fcbdb98da009c1b72ecec3e9345
raphael-deletoille <raphael.deletoille-ext@st.com>
2022-04-07 10:14:57 +0200

    Add Modules

---
+++ ipssi_avril/Modules/logs/variables.tf
@@ -0,0 +1,15 @@
+variable "target_resource_name" {
+    type = string
+}
+
+variable "target_resource_id" {
+    type = string
+}
+
+variable "log_analytics_workspace_name" {
+    type = string
+}
+
+variable "log_analytics_workspace_id" {
+    type = string
+}
